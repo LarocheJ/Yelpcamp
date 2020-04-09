8a40aab19e5e77f738e010281a6b4da71aedb628
root <jimmy.lar91@gmail.com>
2020-04-09 19:24:19 +0000

    Made port variable

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
-app.listen(3000, () => {
+var port = server.listen(process.env.PORT || 3000);
+app.listen(port, () => {
    console.log("The YelpCamp Server Has Started!");
 });
